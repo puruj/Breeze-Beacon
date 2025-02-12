e2126406925c235c49aa898ad854be5b97c7ad24
puruj <puru123456789@hotmail.com>
2025-02-12 18:47:43 -0500

    added tanstack query and made header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,35 @@
+import React from 'react';
+import { useTheme } from '@/context/theme-provider';
+import { Link } from 'react-router-dom';
+import { Moon, Sun } from "lucide-react";
 const Header = () => { 
-    return <header>Header</header>
+    const {theme, setTheme} = useTheme();
+    const isDark = theme === "dark";
+
+    return <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/65'>
+        <div className='container mx-auto flex h-16 items-center justify-between px-4'>
+            <Link to={'/'}>
+            <img src={isDark ? "BB_Light_Logo.png" : "BB_Dark_Logo.png" } 
+            alt="Breeze Beacon Logo" 
+            className="h-14" />
+            </Link>
+            <div>
+                <div
+                    onClick={() => setTheme(isDark ? "light" : "dark")}
+                    className={`flex items-center cursor-pointer transition-transform duration-250 ${
+                        isDark ? "rotate-180" : "rotate-0"
+                    }`}
+                    >
+                    {isDark ? (
+                        <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
+                    ) : (
+                        <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
+                    )}
+                    <span className="sr-only">Toggle theme</span>
+                </div>
+            </div>
+        </div>
+    </header>
 };
 
 export default Header;
