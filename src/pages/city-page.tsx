a7b770bc91ba827b6abc8abf5fd3fe630f1e93eb
puruj <puru123456789@hotmail.com>
2025-02-19 00:23:12 -0500

    city page is functional now

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,5 +1,65 @@
+import { useSearchParams, useParams } from 'react-router-dom';
+import React from 'react';
+import { useForecastQuery, useWeatherQuery } from '@/hooks/use-weather';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertCircle } from 'lucide-react';
+import WeatherSkeleton from '@/components/loading-skeleton';
+import CurrentWeather from '@/components/current-weather';
+import HourlyTemperature from '@/components/hourly-temperature';
+import WeatherDetails from '@/components/weather-details';
+import { WeatherForecast } from '@/components/weather-forcast';
+
 const CityPage = () => {
-    return <div> City Page </div>
-}
+
+    const[searchParams] = useSearchParams();
+    const params =  useParams();
+    const lat = parseFloat(searchParams.get('lat') || 0);
+    const lon = parseFloat(searchParams.get('lon') || 0);
+
+    const coordinates = {lat, lon};
+
+    const weatherQuery = useWeatherQuery(coordinates);
+    const forecastQuery = useForecastQuery(coordinates);
+
+    if(weatherQuery.error || forecastQuery.error) {
+        return (
+            <Alert variant="destructive">
+            <AlertCircle className="h-4 w-4" />
+            <AlertTitle>Location Error</AlertTitle>
+            <AlertDescription className='flex flex-col gap-4'>
+                <p>Failed to fetch weather data, please try again.</p>            
+            </AlertDescription>
+        </Alert>
+        );
+    }
+
+    if(!weatherQuery.data || !forecastQuery.data || !params.cityName) {
+        return <WeatherSkeleton/>
+    }
+
+
+    return (
+        <div className="space-y-4">
+        <div className="flex justify-between items-center">                
+            <h1 className="text-3xl font-bold tracking-tight">{params.cityName}, {weatherQuery.data.sys.country}</h1>
+            <div>
+                {/* fav buton */}
+            </div>
+        </div>
+
+        <div className="grid gap-6">
+            <div className="flex flex-col gap-4">
+                <CurrentWeather data={weatherQuery.data} />
+                <HourlyTemperature data= {forecastQuery.data}/>
+            </div>
+
+            <div className="grid gap-6 md:grid-cols-2 items-start">
+                <WeatherDetails data={weatherQuery.data} />
+                <WeatherForecast data={forecastQuery.data} />
+            </div>
+        </div>
+        </div>
+    )
+};
 
 export default CityPage;
