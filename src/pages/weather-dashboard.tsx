77e2f17073d100c675faf4d306a585cec1892988
puruj <puru123456789@hotmail.com>
2025-02-25 13:57:34 -0500

    fixed import issues in weather dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,13 +1,13 @@
-import CurrentWeather from '@/components/current-weather';
-import { FavoriteCities } from '@/components/favorite-cities';
-import HourlyTemperature from '@/components/hourly-temperature';
-import WeatherSkeleton from '@/components/loading-skeleton';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { button as Button } from "@/components/ui/button"
-import WeatherDetails from '@/components/weather-details';
-import { WeatherForecast } from '@/components/weather-forcast';
-import { useGeolocation } from '@/hooks/use-geolocation';
-import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from '@/hooks/use-weather';
+import CurrentWeather from '@/components/current-weather.js';
+import { FavoriteCities } from '@/components/favorite-cities.js';
+import HourlyTemperature from '@/components/hourly-temperature.js';
+import WeatherSkeleton from '@/components/loading-skeleton.js';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert.js';
+import { Button } from '@/components/ui/button.js';
+import WeatherDetails from '@/components/weather-details.js';
+import { WeatherForecast } from '@/components/weather-forcast.js';
+import { useGeolocation } from '@/hooks/use-geolocation.js';
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from '@/hooks/use-weather.js';
 import { RefreshCw, MapPin, AlertCircle } from 'lucide-react';
 import React from 'react';
 
